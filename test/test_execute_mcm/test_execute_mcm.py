d59d898b54dbfde9ab15a60cd1a39e5609617c39
usamisaori <usamisaori@mail.com>
2024-03-04 23:17:19 +0800

    update test_execute_mcm

--- test/test_execute_mcm/test_execute_mcm.py
+++ test/test_execute_mcm/test_execute_mcm.py
@@ -3,9 +3,15 @@ sys.path.append('../../')
 
 from qcpm import Circuit, Mapper
 
+
+originOutput = sys.stdout
 print('Load circuit data and execute mapping(MCM): \n')
 
 mapper = Mapper()
+
+
+file = open('execute_mcm_ibm.txt', 'w')
+sys.stdout = file
 
 # load circuit(system: IBM)
 circuit_path = 'data_ibm.qasm'
@@ -16,8 +22,8 @@ mapper.execute(circuit, strategy='MCM')
 circuit.save('data_ibm_after.qasm')
 
 
-print('-' * 50 + '\n')
-
+file = open('execute_mcm_surface.txt', 'w')
+sys.stdout = file
 
 # load circuit(system: Surface)
 circuit_path = 'data_surface.qasm'
@@ -26,3 +32,5 @@ circuit = Circuit(circuit_path, system='Surface')
 mapper.execute(circuit, strategy='MCM')
 
 circuit.save('data_surface_after.qasm')
+
+sys.stdout = originOutput
