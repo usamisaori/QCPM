5f2fc4c2fb85c72052954211f2673ef2e8fe2d61
usamisaori <usamisaori@mail.com>
2024-03-04 23:20:26 +0800

    update test_execute_compare

---
+++ test/test_execute_compare/test_execute_compare.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('../../')
+
+from qcpm import Circuit, Mapper
+
+
+originOutput = sys.stdout
+print('Compare different strategy using to execute mapping: \n')
+mapper = Mapper()
+
+
+file = open('execute_normal_strategy.txt', 'w')
+sys.stdout = file
+
+# load circuit(system: IBM)
+circuit_path = 'data_ibm.qasm'
+print(f'Try execute mapping on <{circuit_path}>: ')
+
+# call optimize - mapping .. for two rounds.
+# remember that when init Cirucit do the first optimization.
+
+# normal strategy:
+circuit = Circuit(circuit_path)
+mapper.execute(circuit, silence=True)
+circuit.optimize()
+mapper.execute(circuit, silence=True)
+print(circuit.info)
+
+
+file = open('execute_mcm_strategy.txt', 'w')
+sys.stdout = file
+
+# MCM based mapping:
+circuit = Circuit(circuit_path)
+mapper.execute(circuit, strategy='MCM', silence=True)
+circuit.optimize()
+mapper.execute(circuit, strategy='MCM', silence=True)
+print(circuit.info)
+
+sys.stdout = originOutput
