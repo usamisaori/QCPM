277b11309fb8611cd5827342154e2e45208d41f6
usamisaori <usamisaori@mail.com>
2024-03-04 22:58:56 +0800

    update test_info

--- test/test_info.py
+++ test/test_info.py
@@ -1,7 +1,13 @@
+import sys
+sys.path.append('../')
+
 from qcpm.circuit import CircuitInfo
 
-circuit_path = '../data/example.qasm'
 
+circuit_path = './data/example.qasm'
 circuitInfo = CircuitInfo.fromQASM(circuit_path)
 
-print(circuitInfo.SQG)
+print('Circuit Info: ', circuitInfo)
+
+print('\nused single qubit gates: ', circuitInfo.SQG)
+print('\nused multi qubits gates: ', circuitInfo.MQG)
