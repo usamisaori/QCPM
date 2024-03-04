024b7f0c79952d7db214563ce9d1fd03e0f53004
usamisaori <usamisaori@mail.com>
2024-03-04 23:33:53 +0800

    update test_migration

--- test/test_migration/test_migration.py
+++ test/test_migration/test_migration.py
@@ -3,18 +3,20 @@ sys.path.append('../../')
 
 from qcpm import Circuit
 
+
 print('Migration test \n')
 
 # load circuit(system: IBM)
-circuit_path = 'data_ibm.qasm'
+circuit_path = '../data/data_ibm.qasm'
 print(f'Try load <{circuit_path}>: ')
 circuit = Circuit(circuit_path) # default system='IBM'
 
-circuit.save('data_ibm_after.qasm', to='Surface')
+circuit.save('data_ibm_after.qasm', system='Surface')
+
 
 # load circuit(system: IBM)
-circuit_path = 'data_surface.qasm'
+circuit_path = '../data/data_surface.qasm'
 print(f'Try load <{circuit_path}>: ')
 circuit = Circuit(circuit_path)
 
-circuit.save('data_surface_after.qasm', to='IBM')
+circuit.save('data_surface_after.qasm', system='IBM')
